98b35bd01641a2d9f2a9048637b244d48e695145
Murtuza <34296103+murtazaalam@users.noreply.github.com>
2023-05-28 00:02:03 +0530

    secod

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var dotenv = require('dotenv');
 dotenv.config();
+var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
 
 var mongoUrl = process.env.MongoLiveUrl;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+// app.use(cors());
 var port = process.env.PORT || 9592;
 var db;
 
@@ -15,10 +19,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/movies', (req, res) => {
+    console.log('Movies')
     db.collection('movies').find().toArray((err, result)=>{
         if(err) throw err;
         res.send(result);
     })
+})
+
+app.post('/add-movies', (req, res) => {
+    if(req.body.name && req.body.rating && 
+        req.body.releaseDate && req.body.image){
+            db.collection('movies').insertOne(req.body, (err, result) => {
+                if(err) throw err;
+                res.send({status: 200, message: 'Data Inserted Successfully'});
+            })
+    }
+    else{
+        res.send({status: 400, error: 'Invalid Data'});
+    }
+    
 })
 
 //connecting to mongodb
